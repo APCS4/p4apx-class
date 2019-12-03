aa391314331ffaf1b06cf074a938c83a754dd504
1855970 <1855970@20.8.20.169>
2019-12-03 14:14:57 -0800

    !

--- AP_Exam/BobaQuestions.java
+++ AP_Exam/BobaQuestions.java
@@ -3,10 +3,10 @@ package AP_Exam;
 import java.util.Random;
 
 /**
- * Write a description of class MathOps here.
+ * Grace Liu and Cynthia Li's boba questions basically math stuff
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Team Boba
+ * @version 6.9
  */
 public class BobaQuestions extends Question
 { 
