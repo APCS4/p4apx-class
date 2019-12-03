afdb6ed8f3dc685027497d838f5ad9dfc238b102
1861736 <1861736@20.8.20.100>
2019-12-03 14:10:28 -0800

    Fix

--- AP_UI.java
+++ AP_UI.java
@@ -29,7 +29,6 @@ public class AP_UI extends MenuControl {
     private JTextArea testQuestion = new JTextArea();
     private JTextArea testChoices = new JTextArea();
     private JTextArea testQuestionAnswer = new JTextArea();
-    private final Action action = new SwingAction();
 	
 	/**
 	 * Create the frame.
@@ -176,7 +175,6 @@ public class AP_UI extends MenuControl {
 		contentPane.add(btnArraylist);
 		
 		JButton btnTeamboba = new JButton("✧･ﾟ: *✧･ﾟ:* Team Boba *:･ﾟ✧*:･ﾟ✧");
-		btnTeamboba.setAction(action);
 		btnTeamboba.setForeground(Color.RED);
 		btnTeamboba.setBackground(Color.RED);
 		btnTeamboba.setFont(new Font("Papyrus", Font.BOLD, 16));
@@ -211,8 +209,10 @@ public class AP_UI extends MenuControl {
 		contentPane.add(Power);
 	
 		//alex and mithil code
+	
+	
 
-
+	
 	}
 	
 	}
