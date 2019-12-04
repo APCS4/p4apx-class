53010e6eb2c69b07a2cd33924a6099299463d7ba
Lucy Gao <real.lucy.gao@gmail.com>
2019-12-04 12:51:27 -0800

    new question

--- AP_Exam/CA.java
+++ AP_Exam/CA.java
@@ -6,7 +6,7 @@ public class CA extends Question{
     	setupQuestion();
     }
 	public void setupQuestion() {
-		int random = (int) Math.floor(Math.random()*5);
+		int random = (int) Math.floor(Math.random()*6);
 		switch(random){
 		case 0:
 			this.question = "Refer to the following code:\nswitch(ans)\n{\n     case 1:\n          ans = 11;\n          break;\n     case 2:\n          ans = 78;\n          break;\n}\n\nIf ans is 1, what is it after the switch case?";
@@ -52,6 +52,24 @@ public class CA extends Question{
 			this.choiceD = "int[][]  D  =  {{3,4,8,9\\n6,-1,4,10}};";
 			this.choiceE = "None of the above";
 			this.answerKey = 'A';
+			break;
+		case 5:
+			this.question = "double x = 3.87;\nint y = (int)(x - 0.5);\nSystem.out.println(y);\n\nWhat will be printed?";
+			this.choiceA = "3.87";
+			this.choiceB = "3";
+			this.choiceC = "4";
+			this.choiceD = "The code will result in an error";
+			this.choiceE = "None of the above";
+			this.answerKey = 'D';
+			break;
+		case 6:
+			this.question = "double x = -11.96;\nint y = (int)(x - 0.5);\nSystem.out.println(y);\n\nWhat will be printed?";
+			this.choiceA = "-11";
+			this.choiceB = "-12";
+			this.choiceC = "-11.9";
+			this.choiceD = "-11.96";
+			this.choiceE = "-12.0";
+			this.answerKey = 'B';
 			break;
 		default:
 			System.out.print("ERROR");
