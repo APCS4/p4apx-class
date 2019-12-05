7d5f5a8e71008ba368f957f4cad98c8ac9c72a27
1945874 <1945874@20.8.20.232>
2019-12-05 12:18:31 -0800

    e

--- AP_UI.java
+++ AP_UI.java
@@ -265,7 +265,7 @@ public class AP_UI extends MenuControl {
 		
 		
 		
-		//abcd
+		//abcde
 		//JButton btnArraylist = new JButton("ArrayList");
 		//btnArraylist.setBounds(461, 101, 138, 29);
 		//contentPane.add(btnArraylist);
