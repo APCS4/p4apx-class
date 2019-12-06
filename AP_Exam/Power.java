49982650aa2f29048752e46edc4e63533be6ea08
1831933 <1831933@20.8.20.203>
2019-12-06 14:05:31 -0800

    new changes

--- AP_Exam/Power.java
+++ AP_Exam/Power.java
@@ -36,15 +36,15 @@ public class Power extends Question{
 		 */
 		
 		
-		case 0:
+		case 0://q1
 			
 			
 			this.question = "What Iteration is a controlled by a programmer variable choice";
 			
 			pick = loadArray("Do While","Switch Case", "For", 
-					"If,Else,For", "Thats done by the user");
+					"If,Else,For", "Thats done by the user");//array for stroing answers
 			
-			this.choiceA = pick[0];
+			this.choiceA = pick[0];//choices A-E loaded in
 			this.choiceB = pick[1];
 			this.choiceC = pick[2];
 			this.choiceD = pick[3];
@@ -53,24 +53,24 @@ public class Power extends Question{
 			
 			
 			this.answerKey = Ans(pick,"For");
-			this.answer = "U got it homes";
+			this.answer = "U got it homes";//encouragement
 			break;
 		
-		case 1:
+		case 1://q2
 			this.question = "What is the correct format for writing a for loop?";
 			pick=loadArray("For:", "for(int z=12; z<=103; z--);" ,"for{in i=1: i<6:}", "for {int i = 1: i<=6:} ()", "for: a=1, b=2, i++" );
 			
-			this.choiceA = pick[0];
+			this.choiceA = pick[0];//loaded in answers
 			this.choiceB = pick[1];
 			this.choiceC = pick[2];
 			this.choiceD = pick[3];
 			this.choiceE = pick[4];
 			
-			this.answerKey = Ans(pick,"for(int z=12; z<=103; z--);");
+			this.answerKey = Ans(pick,"for(int z=12; z<=103; z--);");//correct answer
 			this.answer = "U got it homes";
 			break;
 		
-		case 2:
+		case 2://q3
 			
 			this.question = "Which of the following is most closely associated with iterations?";
 			pick=loadArray("loops such as for loops","Recursion","Instance Initialization","Public Static Void","if else statements");
@@ -83,7 +83,7 @@ public class Power extends Question{
 			this.answerKey = Ans(pick,"loops such as for loops");
 			this.answer = "U got it homes";
 			break;
-		case 3:
+		case 3://q4
 		
 			this.question = "What is the function of a do while loop?";
 			pick=loadArray("to replace a for loopo in all cases for shortening code","Recursion only","Executing a block of code at least once and then repeatedly executing it based on a boolean condition","Allowing code to be repeatedly executed","To create simultaneous control flows for simplified code");
@@ -97,7 +97,7 @@ public class Power extends Question{
 			this.answer = "U got it homes";
 			break;
 		
-		case 4:
+		case 4://q5
 		
 			this.question = "Consider the following code segment:\n for (int p = 1; p <= 100; k++)\n + if ((p % 4) == 0)\n + System.out.println(p);";
 			pick=loadArray("for(intp=1;p<=25;p++)\n + System.out.println(p);", "for(intp=1;p<=100;p=p+4)\n System.out.println(p);", "for(intp=1;p<=100;p--)\n System.out.println(p % 9);", "for(intp=4;p<=25;p=19*p)\n System.out.println(p);", "for(intp=4;p<=100;p=p+4)\n System.out.println(p);");
@@ -112,13 +112,13 @@ public class Power extends Question{
 		
 		
 		default:
-			System.out.print("elproblem");
+			System.out.print("elproblem");//default case issue
 		
 		
 		}
 	}
 
-	private String[] loadArray (String ques1, String ques2, String ques3, String ques4, String ques5)
+	private String[] loadArray (String ques1, String ques2, String ques3, String ques4, String ques5)//all choices passed as an argument
 	{
 		/**
 		 *  loadarray is used to take the 4 answer choices and randomize them in an array, then return the choice
@@ -134,13 +134,13 @@ public class Power extends Question{
 		
 		//load questions into array
 		pick[0] = ques1;
-		pick[1] = ques2;
+		pick[1] = ques2;//load possible choices
 		pick[2] = ques3;
 		pick[3] = ques4;
 		pick[4] = ques5;
 		
 		
-		for(int i = 0; i <5; i++)
+		for(int i = 0; i <5; i++)//bubble sort!!! for choice
 		{
 			hold = pick[i];
 			rando = (int) Math.floor(Math.random() * 5);
@@ -148,7 +148,7 @@ public class Power extends Question{
 			pick[rando] = hold;
 			
 		}
-		return pick;	
+		return pick;	//retuns the answers randomized
 	}
 	
 	/**
@@ -159,10 +159,10 @@ public class Power extends Question{
 	 * @return
 	 */
 	private char Ans(String ques[], String an) {
-		char ret;
+		char ret;//check the correct versus input
 		
 		
-		if(ques[0].equals(an) == true)
+		if(ques[0].equals(an) == true)//see if the answer choice is correct, and return
 			ret = 'A';
 		else if(ques[1].equals(an) == true)
 			ret = 'B';
@@ -172,7 +172,7 @@ public class Power extends Question{
 			ret = 'D';
 		else if(ques[4].equals(an) == true)
 			ret = 'E';
-		else ret = 'Z'; 
+		else ret = 'Z'; //error problem problem
 		
 		return ret;
 	}
