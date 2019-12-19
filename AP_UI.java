f1af31be510261f347d98526cd29f6433fe06d21
Evan Knaggs <1916691@20.8.20.81>
2019-12-19 12:53:29 -0800

    removed unused imports

--- AP_UI.java
+++ AP_UI.java
@@ -1,15 +1,8 @@
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-
-import AP_Exam.AP_Console;
-
-
-import AP_Exam.encapsulationQuestions;
 
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
@@ -18,15 +11,9 @@ import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
 import java.awt.Color;
 import java.awt.Font;
-import javax.swing.JSeparator;
 import javax.swing.JTextArea;
-import javax.swing.JTextField;
-import javax.swing.AbstractAction;
-import javax.swing.Action;
-import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
 import javax.swing.ImageIcon;
-import java.awt.Dimension;
 
 public class AP_UI extends MenuControl 
 {
