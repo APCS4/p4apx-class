07e3668eba8c2c7796eae9768f22f9a5a22d7f27
aashi <aashimittal19@gmail.com>
2020-01-13 13:15:54 -0800

    new class

--- TestModeLogic.java
+++ TestModeLogic.java
@@ -1,4 +1,20 @@
 
 public class TestModeLogic extends MenuControl{
+	
+	
+	public TestModeLogic()
+	{
+		
+	}
+	
+	
+	
+	public void makeQuestions()
+	{
+		AP_Exam.Question [] questionList = new AP_Exam.Question [13]; 
+		AP_Exam.Question type1 = new AP_Exam.ArrayListQuestions(); 
+
+		
+	}
 
 }
