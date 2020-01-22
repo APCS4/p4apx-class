c4f24b3bf447869bbef3d37a0add7397e9439e47
CynthiaLi <1855970@20.8.20.169>
2020-01-22 12:49:22 -0800

    added rando op

--- AP_Exam/Randomization.java
+++ AP_Exam/Randomization.java
@@ -1,5 +1,6 @@
 package AP_Exam;
 
+import java.lang.reflect.Array;
 import java.util.Random;
 
 public class Randomization {
@@ -17,13 +18,35 @@ public class Randomization {
 		return i;
 	}
 	
+	public static int randoIntRange(int n, int m)
+	{
+		Integer i = n + rando.nextInt(m-n);
+		return i;
+	}
+	
 	public static Double randoDouble()
 	{
 		Double i = rando.nextDouble();
 		return i;
 	}
 	
-	public static randoArray(i)
+	public static Object randoOp()
+	{
+		Object[] oper;
+		
+		oper[0] = "+";
+		oper[1] = "-";
+		oper[2] = "/";
+		oper[3] = "*";
+		oper[4] = "%";
+		
+		int a = randoInt(4);
+		
+		
+		return oper[a];
+	}
+	
+	public static randoArray(n)
 	{
 		ArrayList list = new ArrayList();
 	}
