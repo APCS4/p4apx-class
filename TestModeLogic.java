f0fb180c4fe40fdefbad482b292830f3c4710e16
aashi <aashimittal19@gmail.com>
2020-01-27 12:48:29 -0800

    changes

--- TestModeLogic.java
+++ TestModeLogic.java
@@ -5,30 +5,40 @@ import model_questions.QuestionMC;
 
 public class TestModeLogic /*extends MenuControl */{
 	
-	int trackQCalls[] = {0,0,0,0,0,0,0,0,0}; //tracks # of times each class was called, starts at 0 (array has 13 spots, ranges from 0-12)
-	public int testQs = 5;
-	public int testQsFields = 20;				// number of fields in question
+	
+
+	public int testQs = 24; //THIS CAN CHANGE
+	public int testQsFields = 8;				// number of fields in question, THIS CAN'T CHANGE 
 	public String [][] test = new String[testQs][testQsFields]; //creating a new 2-D String array that will hold in each column: a question type object, and its corresponding 
 
+	int trackQCalls[] = new int [6];//tracks # of times each class was called, starts at 0 (array has 13 spots, ranges from 0-12)
+	
 
 	
 	public TestModeLogic()
 	{
+		
 		makeQuestions();
 	}
 	
 	
 	public void makeQuestions()
 	{
+		
+		for(int i = 0; i <6; i++)
+		{
+			trackQCalls[i]=0; 
+		}
+		
 		Question [] questionList = {
-				/*new AP_Exam.FinalMath(),
-				new AP_Exam.FinalCodeAnalysis(), */
+				//new AP_Exam.FinalMath(),
+				new AP_Exam.FinalCodeAnalysis(), 
 				new AP_Exam.FinalBooleanQuestions(),
 				new AP_Exam.FinalInfoQuestions(),
 				new AP_Exam.FinalLoopQuestion(), 
 				new AP_Exam.FinalPolymorphQuestions(), 
 				//new AP_Exam.FinalRecursion(), //has error 
-				new AP_Exam.FinalArrayList(), //perfect */
+				new AP_Exam.FinalArrayList(), //perfect 
 				//new AP_Exam.finalStrings() 
 		};
 
