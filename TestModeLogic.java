fcd2c788c29c3e38024cfe2a86a008c6f58e1d79
aashi <aashimittal19@gmail.com>
2020-01-26 22:00:21 -0800

    lots of null errors, scrolling not working, need to add button groups, score report. questions not printing right

--- TestModeLogic.java
+++ TestModeLogic.java
@@ -6,9 +6,10 @@ import model_questions.QuestionMC;
 public class TestModeLogic /*extends MenuControl */{
 	
 	int trackQCalls[] = {0,0,0,0,0,0,0,0,0}; //tracks # of times each class was called, starts at 0 (array has 13 spots, ranges from 0-12)
-	public String [][] test = new String[36][8]; //creating a new 2-D String array that will hold in each column: a question type object, and its corresponding 
 	public int testQs = 4;
-	public int testQsFields = 1;				// number of fields in question
+	public int testQsFields = 24;				// number of fields in question
+	public String [][] test = new String[testQs][testQsFields]; //creating a new 2-D String array that will hold in each column: a question type object, and its corresponding 
+
 
 	
 	public TestModeLogic()
@@ -23,14 +24,14 @@ public class TestModeLogic /*extends MenuControl */{
 		// array of exam questions
 		Question [] questionList = {
 				/*new AP_Exam.FinalMath(),
-				new AP_Exam.FinalCodeAnalysis(),
+				new AP_Exam.FinalCodeAnalysis(), */
 				new AP_Exam.FinalBooleanQuestions(),
-				new AP_Exam.finalInfoQuestions(),
-				new AP_Exam.FinalLoopQuestion(), */
-				//new AP_Exam.FinalPolymorphQuestions(), 
+				new AP_Exam.FinalInfoQuestions(),
+				new AP_Exam.FinalLoopQuestion(), 
+				new AP_Exam.FinalPolymorphQuestions(), 
 				//new AP_Exam.FinalRecursion(), //has error 
-				new AP_Exam.FinalArrayList(), //perfect 
-				//new AP_Exam.finalStrings() 
+				new AP_Exam.FinalArrayList(), //perfect */
+				new AP_Exam.finalStrings() 
 		};
 		
 		// 2D array management values
@@ -83,7 +84,7 @@ public String printAll()
 		String fullTest = ""; 
 		for (int x=0; x< testQs; x++)
 		{
-			for(int y = 0; y< testQsFields; y++)
+			for(int y = 0; y< testQsFields-2; y++)
 			
 				
 				fullTest += getTestElement(x, y) + "\n\n";
